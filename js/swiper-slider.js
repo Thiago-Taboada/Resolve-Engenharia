4baca3ecf4f43ffc6addf532489e7650eeae6d91
Thiago Taboada <contact.thiago.taboada@gmail.com>
2022-10-06 17:02:13 -0300

    Add slide (test)

---
+++ js/swiper-slider.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    loop: true,
+    speed: 500,
+  
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
